2c48c57172c4165941063f0ced0766b229c839a5
Larry Bank <bitbank@pobox.com>
2020-03-23 17:49:04 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+OneBitDisplay (1-bpp OLED/LCD library)<br>
+-----------------------------------
+Project started 3/23/2020<br>
+Copyright (c) 2020 BitBank Software, Inc.<br>
+Written by Larry Bank<br>
+bitbank@pobox.com<br>
+<br>
+![OneBitDisplay](/demo.jpg?raw=true "OneBitDisplay")
+<br>
+The purpose of this code is to easily control SH1106/SSD1306 OLED
+displays using a minimum of FLASH and RAM. The displays can be connected to
+the traditional I2C bus or you can use any 2 GPIO pins to define a virtual
+I2C bus. On AVR microcontrollers, there is an optimized option to speed up
+access to the GPIO pins to allow speeds which match or exceed normal I2C
+speeds. The pins are numbered with the Port letter as the first digit followed
+by the bit number. For example, To use bit 0 of Port B, you would reference
+pin number 0xb0. Includes the unique feature that the init function can optionally detect the display address (0x3C or 0x3D) and the controller type (SSD1306, SH1106 or
+SH1107).<br>
+I try to support as many OLEDs as I can. I was able to justify buying a bunch
+of different sized SSD1306 displays because they're around $2 each. A generous patron
+donated money so that I could purchase Pimoroni's 128x128 OLED and add support for it.
+It uses the SH1107 controller and behaves very similarly to the SH1106.
+<br>
+
+Features:<br>
+---------<br>
+- Supports any number of simultaneous displays of any type (mix and match)<br>
+- Optionally detect the display address and type (I2C only)<br>
+- Supports 72x40, 96x16, 64x32, 128x32, 128x64, 128x128 (SH1107) and 132x64 (SH1106) display sizes<br>
+- Drive displays from I2C, SPI or any 2 GPIO pins (virtual I2C)<br>
+- 4 sizes of fixed fonts (6x8, 8x8, 16x16, 16x32)<br>
+- Deferred rendering allows preparing a back buffer, then displaying it (usually faster)<br>
+- Text scrolling features (vertical and horizontal)<br>
+- Text cursor position with optional line wrap<br>
+- a function to load a Windows BMP file<br>
+- Pixel drawing on SH1106/7 without needing backing RAM<br>
+- Optimized Bresenham line drawing<br>
+- Optimized Bresenham outline and filled ellipse drawing<br>
+- Optimized outline and filled rectangle drawing<br>
+- Optional backing RAM for drawing pixels for systems with enough RAM<br>
+- 16x16 Tile/Sprite drawing at any angle.<br>
+- Run full frame animations at high frame rates with a simple API<br>
+- Light enough to run on an ATtiny85<br> 
+<br>
+This code depends on the BitBang_I2C library. You can download it here:<br>
+<br>
+https://github.com/bitbank2/BitBang_I2C
+<br>
+
+Instructions for use:<br>
+---------------------<br>
+Start by initializing the library. Either using hardware I2C, bit-banged I2C or SPI to talk to the display. For I2C, the
+address of the display will be detected automatically (either 0x3c or 0x3d). The typical MCU only allows setting the I2C speed up to 400Khz, but the SSD1306 displays can handle a much faster signal. With the bit-bang code, you can usually specify a stable 800Khz clock and with Cortex-M0 targets, the hardware I2C can be told to be almost any speed, but the displays I've tested tend to stop working beyond 1.6Mhz. After initializing the display you can begin drawing text or graphics on it. The final parameter of all of the drawing functions is a render flag. When true, the graphics will be sent to the internal backing buffer (when available) and sent to the display. You optionally pass the library a backing buffer (if your MCU has enough RAM) with the oledSetBackBuffer() function. When the render flag is false, the graphics will only be drawn into the internal buffer. Once you're ready to send the pixels to the display, call oledDumpBuffer(NULL) and it will copy the internal buffer in its entirety to the display. The text drawing function now has a scroll offset parameter. This tells it how many pixels of the text to skip before drawing the text at the given destination coordinates. For example, if you pass a value of 20 for the scroll offset and are using an 8-pixel wide font (FONT_NORMAL), the first two and a half characters will not be drawn; the second half of the third and subsequent characters will be drawn starting at the x/y you specified. This allows you to create a scrolling text effect by repeatedly calling the oledWriteString() function with progressively larger scroll offset values to make the text scroll from right to left.<br> 
+
+If you find this code useful, please consider sending a donation.
+
+[![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=SR4F44J2UR8S4)
+
---
+++ src/OneBitDisplay.h
@@ -0,0 +1,229 @@
+#ifndef __ONEBITDISPLAY__
+#define __ONEBITDISPLAY__
+
+#include <BitBang_I2C.h>
+
+typedef struct obdstruct
+{
+uint8_t oled_addr; // requested address or 0xff for automatic detection
+uint8_t oled_wrap, oled_flip, oled_type;
+uint8_t *ucScreen;
+uint8_t iCursorX, iCursorY;
+uint8_t oled_x, oled_y;
+int iScreenOffset;
+BBI2C bbi2c;
+uint8_t iDCPin, iMOSIPin, iCLKPin, iCSPin;
+uint8_t iLEDPin; // backlight
+uint8_t bBitBang;
+} OBDISP;
+// Make the Linux library interface C instead of C++
+#if defined(_LINUX_) && defined(__cplusplus)
+extern "C" {
+#endif
+
+typedef enum
+{
+ MODE_DATA = 0,
+ MODE_COMMAND
+} DC_MODE;
+
+// These are defined the same in my SPI_LCD library
+#ifndef SPI_LCD_H
+
+// 4 possible font sizes: 8x8, 16x32, 6x8, 16x16 (stretched from 8x8)
+enum {
+   FONT_NORMAL = 0,
+   FONT_LARGE,
+   FONT_SMALL,
+   FONT_STRETCHED
+};
+#endif
+
+// Display type for init function
+enum {
+  OLED_128x128 = 1,
+  OLED_128x32,
+  OLED_128x64,
+  OLED_132x64,
+  OLED_64x32,
+  OLED_96x16,
+  OLED_72x40,
+  LCD_UC1701,
+  LCD_HX1230,
+  LCD_NOKIA5110
+};
+
+// Rotation and flip angles to draw tiles
+enum {
+  ANGLE_0=0,
+  ANGLE_90,
+  ANGLE_180,
+  ANGLE_270,
+  ANGLE_FLIPX,
+  ANGLE_FLIPY
+};
+
+// Return value from obdInit()
+enum {
+  OLED_NOT_FOUND = -1, // no display found
+  OLED_SSD1306_3C,  // SSD1306 found at 0x3C
+  OLED_SSD1306_3D,  // SSD1306 found at 0x3D
+  OLED_SH1106_3C,   // SH1106 found at 0x3C
+  OLED_SH1106_3D,   // SH1106 found at 0x3D
+  OLED_SH1107_3C,  // SH1107
+  OLED_SH1107_3D,
+  LCD_OK,
+  LCD_ERROR
+};
+//
+// Initializes the display controller into "page mode" on I2C
+// If SDAPin and SCLPin are not -1, then bit bang I2C on those pins
+// Otherwise use the Wire library.
+// If you don't need to use a separate reset pin, set it to -1
+//
+int obdInit(OBDISP *pOBD, int iType, int iAddr, int bFlip, int bInvert, int bWire, int iSDAPin, int iSCLPin, int iResetPin, int32_t iSpeed);
+//
+// Initialize an SPI version of the display
+//
+void obdSPIInit(OBDISP *pOBD, int iType, int iDC, int iCS, int iReset, int iMOSI, int iCLK, int bFlip, int bInvert, int32_t iSpeed);
+
+//
+// Provide or revoke a back buffer for your OLED graphics
+// This allows you to manage the RAM used by ss_oled on tiny
+// embedded platforms like the ATmega series
+// Pass NULL to revoke the buffer. Make sure you provide a buffer
+// large enough for your display (e.g. 128x64 needs 1K - 1024 bytes)
+//
+void obdSetBackBuffer(OBDISP *pOBD, uint8_t *pBuffer);
+//
+// Sends a command to turn off the OLED display
+//
+void obdShutdown();
+//
+// Sets the brightness (0=off, 255=brightest)
+//
+void obdSetContrast(OBDISP *pOBD, unsigned char ucContrast);
+//
+// Load a 1-bpp Windows bitmap
+// Pass the pointer to the beginning of the BMP file
+// First pass version assumes a full screen bitmap
+//
+int obdLoadBMP(OBDISP *pOBD, uint8_t *pBMP, int bInvert, int bRender);
+//
+// Power up/down the display
+// useful for low power situations
+//
+void obdPower(OBDISP *pOBD, uint8_t bOn);
+//
+// Set the current cursor position
+// The column represents the pixel column (0-127)
+// The row represents the text row (0-7)
+//
+void obdSetCursor(OBDISP *pOBD, int x, int y);
+
+//
+// Turn text wrap on or off for the obdWriteString() function
+//
+void obdSetTextWrap(OBDISP *pOBD, int bWrap);
+//
+// Draw a string of normal (8x8), small (6x8) or large (16x32) characters
+// At the given col+row with the given scroll offset. The scroll offset allows you to
+// horizontally scroll text which does not fit on the width of the display. The offset
+// represents the pixels to skip when drawing the text. An offset of 0 starts at the beginning
+// of the text.
+// The system remembers where the last text was written (the cursor position)
+// To continue writing from the last position, set the x,y values to -1
+// The text can optionally wrap around to the next line by calling oledSetTextWrap(true);
+// otherwise text which would go off the right edge will not be drawn and the cursor will
+// be left "off screen" until set to a new position explicitly
+//
+//  Returns 0 for success, -1 for invalid parameter
+//
+int obdWriteString(OBDISP *pOBD, int iScrollX, int x, int y, char *szMsg, int iSize, int bInvert, int bRender);
+//
+// Fill the frame buffer with a byte pattern
+// e.g. all off (0x00) or all on (0xff)
+//
+void obdFill(OBDISP *pOBD, unsigned char ucData, int bRender);
+//
+// Set (or clear) an individual pixel
+// The local copy of the frame buffer is used to avoid
+// reading data from the display controller
+// (which isn't possible in most configurations)
+// This function needs the USE_BACKBUFFER macro to be defined
+// otherwise, new pixels will erase old pixels within the same byte
+//
+int obdSetPixel(OBDISP *pOBD, int x, int y, unsigned char ucColor, int bRender);
+//
+// Dump an entire custom buffer to the display
+// useful for custom animation effects
+//
+void obdDumpBuffer(OBDISP *pOBD, uint8_t *pBuffer);
+//
+// Render a window of pixels from a provided buffer or the library's internal buffer
+// to the display. The row values refer to byte rows, not pixel rows due to the memory
+// layout of OLEDs. Pass a src pointer of NULL to use the internal backing buffer
+// returns 0 for success, -1 for invalid parameter
+//
+int obdDrawGFX(OBDISP *pOBD, uint8_t *pSrc, int iSrcCol, int iSrcRow, int iDestCol, int iDestRow, int iWidth, int iHeight, int iSrcPitch);
+
+//
+// Draw a line between 2 points
+//
+void obdDrawLine(OBDISP *pOBD, int x1, int y1, int x2, int y2, uint8_t ucColor, int bRender);
+//
+// Play a frame of animation data
+// The animation data is assumed to be encoded for a full frame of the display
+// Given the pointer to the start of the compressed data,
+// it returns the pointer to the start of the next frame
+// Frame rate control is up to the calling program to manage
+// When it finishes the last frame, it will start again from the beginning
+//
+uint8_t * obdPlayAnimFrame(OBDISP *pOBD, uint8_t *pAnimation, uint8_t *pCurrent, int iLen);
+
+//
+// Scroll the internal buffer by 1 scanline (up/down)
+// width is in pixels, lines is group of 8 rows
+// Returns 0 for success, -1 for invalid parameter
+//
+int obdScrollBuffer(OBDISP *pOBD, int iStartCol, int iEndCol, int iStartRow, int iEndRow, int bUp);
+//
+// Draw a sprite of any size in any position
+// If it goes beyond the left/right or top/bottom edges
+// it's trimmed to show the valid parts
+// This function requires a back buffer to be defined
+// The priority color (0 or 1) determines which color is painted 
+// when a 1 is encountered in the source image.
+// e.g. when 0, the input bitmap acts like a mask to clear
+// the destination where bits are set.
+//
+void obdDrawSprite(OBDISP *pOBD, uint8_t *pSprite, int cx, int cy, int iPitch, int x, int y, uint8_t iPriority);
+//
+// Draw a 16x16 tile in any of 4 rotated positions
+// Assumes input image is laid out like "normal" graphics with
+// the MSB on the left and 2 bytes per line
+// On AVR, the source image is assumed to be in FLASH memory
+// The function can draw the tile on byte boundaries, so the x value
+// can be from 0 to 112 and y can be from 0 to 6
+//
+void obdDrawTile(OBDISP *pOBD, const uint8_t *pTile, int x, int y, int iRotation, int bInvert, int bRender);
+//
+// Draw an outline or filled ellipse
+//
+void obdEllipse(OBDISP *pOBD, int iCenterX, int iCenterY, int32_t iRadiusX, int32_t iRadiusY, uint8_t ucColor, uint8_t bFilled);
+//
+// Draw an outline or filled rectangle
+//
+void obdRectangle(OBDISP *pOBD, int x1, int y1, int x2, int y2, uint8_t ucColor, uint8_t bFilled);
+
+//
+// Turn the LCD backlight on or off
+//
+void obdBacklight(OBDISP *pODB, int bOn);
+
+#if defined(_LINUX_) && defined(__cplusplus)
+}
+#endif // _LINUX_
+
+#endif // __ONEBITDISPLAY__
+
