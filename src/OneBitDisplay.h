faa3841670a5d2c141b2a6244d67e4f5d85e2fd7
Laurence Bank <laurencebank@Laurences-Air.home>
2022-12-31 15:40:56 +0000

    Refactor for easier porting to embedded systems

--- src/OneBitDisplay.h
+++ src/OneBitDisplay.h
@@ -1,3 +1,18 @@
+//
+// OneBitDisplay (OLED/LCD/E-Paper library)
+// Copyright (c) 2020 BitBank Software, Inc.
+// Written by Larry Bank (bitbank@pobox.com)
+// Project started 3/23/2020
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 #ifndef __ONEBITDISPLAY__
 #define __ONEBITDISPLAY__
 
@@ -107,6 +122,7 @@ enum {
 #endif
   EPD102_80x128, // not working yet
   EPD47_540x960, // not working yet
+  EPD35R_184x384, // Hanshow Nebular 3.5" BWR
   LCD_COUNT
 };
 
